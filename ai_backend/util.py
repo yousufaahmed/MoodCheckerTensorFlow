aabe955862257bb37296eb0180da8f6153bfdd14
Sri Guhan <88606304+guhan-tofu@users.noreply.github.com>
2024-10-27 21:32:24 +0000

    Update util.py

--- ai_backend/util.py
+++ ai_backend/util.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import json
-import pickle
 import torch
 from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
 from model import DistilBERTClass
